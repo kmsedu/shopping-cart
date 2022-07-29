b9782fce0f0763fc944e3c486fb17ea7f86138c6
kris <kmsedu@tuta.io>
2022-07-30 05:38:38 +1000

    Formatted code

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import RouteSwitch from "./layout/RouteSwitch";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import RouteSwitch from './layout/RouteSwitch'
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(<RouteSwitch />);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(<RouteSwitch />)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     gridTemplateColumns: {
-      "auto-fit": "repeat(auto-fill, minmax(400px, 1fr))",
+      'auto-fit': 'repeat(auto-fill, minmax(400px, 1fr))'
     },
     extend: {
       fontFamily: {
         display: '"Expletus Sans"',
-        nav: '"Inder"',
+        nav: '"Inder"'
       },
       backgroundImage: {
-        "shop-hero":
-          "linear-gradient(to bottom, rgba(0, 0, 0, 0), #e5e5e5), url('../public/assets/shop-hero-min.jpg')",
-      },
-    },
+        'shop-hero':
+          "linear-gradient(to bottom, rgba(0, 0, 0, 0), #e5e5e5), url('../public/assets/shop-hero-min.jpg')"
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
