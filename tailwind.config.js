fc9b9f66b47aab923f5d5afe380a8a3039b899a5
kris <kmsedu@tuta.io>
2022-07-29 18:53:00 +1000

    Added grid spacing to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
+    gridTemplateColumns: {
+      'auto-fit': 'repeat(auto-fit, minmax(250px, 1fr))'
+    },
     extend: {
       fontFamily: {
         display: '"Expletus Sans"',
