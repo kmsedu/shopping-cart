488707d677dbf08327cf753bcbbc2fd912cd79be
kris <kmsedu@tuta.io>
2022-07-31 00:31:47 +1000

    Removed absolute path from image data

--- src/data.ts
+++ src/data.ts
@@ -2,56 +2,56 @@ export default [
   {
     id: '5wuzIrxdBNAWiOVvdRgv7',
     name: 'Lenovo 15.6" IdeaPad Slim 3i',
-    imgSrc: '/assets/ideapadslim15i.png',
+    imgSrc: 'assets/ideapadslim15i.png',
     price: 697.0,
     inStock: true
   },
   {
     id: '3FPEdio4ANLoAEHrElAM_',
     name: 'MacBook Air 13.6" M2 Chip Silver',
-    imgSrc: '../assets/macbookair13.png',
+    imgSrc: 'assets/macbookair13.png',
     price: 1707.0,
     inStock: true
   },
   {
     id: 'opl0Q5xNuOZv4gElOCLN5',
     name: 'MacBook Pro 14" 10-Core M2 Silver',
-    imgSrc: '../assets/macbook14.png',
+    imgSrc: 'assets/macbook14.png',
     price: 3347.0,
     inStock: true
   },
   {
     id: 'ttWShh-tJLOMBSqqnSsj-',
     name: 'Macbook Pro 16" 32-Core M2 Max',
-    imgSrc: '../assets/macbook16.png',
+    imgSrc: 'assets/macbook16.png',
     price: 4997.0,
     inStock: true
   },
   {
     id: 'K1XUvFwmidvuzHhtH3yXD',
     name: 'Acer 15.6" Nitro 5 Notebook i7',
-    imgSrc: '../assets/nitro5.png',
+    imgSrc: 'assets/nitro5.png',
     price: 1697.0,
     inStock: true
   },
   {
     id: 'wL5JzmkqCz-f4uKSi4K3A',
     name: 'ASUS 15.6" ROG Strix G15 Laptop',
-    imgSrc: '../assets/strixg15.png',
+    imgSrc: 'assets/strixg15.png',
     price: 1997.0,
     inStock: true
   },
   {
     id: 'UwSwJHKXGJ4VS3h8K94sJ',
     name: 'ASUS 15.6" TUF Gaming Notebook i7',
-    imgSrc: '../assets/tuff15.png',
+    imgSrc: 'assets/tuff15.png',
     price: 2697.0,
     inStock: true
   },
   {
     id: '8jXEebLH7oF4f_fxKsAKw',
     name: 'ASUS 15.6" VivoBook Core i7',
-    imgSrc: '../assets/vivobook15.png',
+    imgSrc: 'assets/vivobook15.png',
     price: 1397.0,
     inStock: true
   }
